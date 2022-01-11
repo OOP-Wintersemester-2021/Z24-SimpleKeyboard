2e0521c94067cc828561cb72f644a34cd722c9c4
Alexander Bazo <alexanderbazo@googlemail.com>
2022-01-11 17:46:38 +0100

    Add comments

--- src/Key.java
+++ src/Key.java
@@ -2,32 +2,50 @@ import config.Theme;
 import de.ur.mi.oop.graphics.Label;
 import de.ur.mi.oop.graphics.Rectangle;
 
+/**
+ * Diese Klasse repräsentiert einen einzelnen Buchstaben bzw. eine einzelne Taste auf dem Bildschirm.
+ * Für jeden Buchstaben wird ein Rechteck gezeichnet, das (fast) zentriert den jeweiligen Buchstaben
+ * als Label beinhaltet.
+ */
 public class Key {
 
     public static final float SIZE = 30;
 
-    public final Rectangle shape;
-    public final Label value;
+    public final Rectangle shape; // "Hintergrund" der Taste
+    public final Label value; // "Beschriftung" der Taste
 
     public Key(float x, float y, char key) {
         shape = new Rectangle(x, y, SIZE, SIZE, Theme.RED);
-        value =  new Label(x + 8, y + 22, String.valueOf(key).toUpperCase(), Theme.GREY);
+        /*
+         * Die Positionierung des Labels mittig auf dem Rechteck erfolgt über Erfahrungswerte auf Basis der verwendeten
+         * Schriftgröße, da die genaue Berechnung der Label-Größe in der GraphicsApp aktuell nur fehlerhaft funktioniert.
+         */
+        value = new Label(x + 8, y + 22, String.valueOf(key).toUpperCase(), Theme.GREY);
         value.setFont("Arial Rounded MT Bold");
         value.setFontSize(14);
     }
 
+    /**
+     * Die Methode hebt je nach aktuellem Zustand entweder die Taste hervor oder entfernt deren Hervorhebung.
+     */
     public void toggleFocus() {
-        if(shape.getColor() == Theme.YELLOW) {
+        if (shape.getColor() == Theme.YELLOW) { // Wenn die Taste bereits hervorgehoben wurde ...
             unfocus();
-        } else {
+        } else { // ... andernfalls
             focus();
         }
     }
 
+    /**
+     * Setzt die farbliche Hervorhebung
+     */
     private void focus() {
         shape.setColor(Theme.YELLOW);
     }
 
+    /**
+     * Entfernt die farbliche Hervorhebung
+     */
     private void unfocus() {
         shape.setColor(Theme.RED);
     }
--- src/SimpleKeyboard.java
+++ src/SimpleKeyboard.java
@@ -7,9 +7,16 @@ import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
 import java.util.HashMap;
 
+/**
+ * In diesem Beispiele stellen wir mehrere Rechtecke auf dem Bildschirm dar, die die Buchstaben-
+ * Tasten auf der Tastatur repräsentieren sollen. Beim Druck auf eine der Tasten wird das entsprechende
+ * Rechteck farblich hervorgehoben. Wird die Taste wieder losgelassen, verschwindet die Hervorhebung. Um
+ * bei einem Tastendruck schnell das passende Rechteck zu finden, speichern wir diese in einer HashMap
+ * und nutzen dabei den char-Wert der Taste (z.B. 'a' für die A-Taste) als Schlüssel.
+ */
 public class SimpleKeyboard extends GraphicsApp {
 
-    private HashMap<Character, Key> keys;
+    private HashMap<Character, Key> keys; // HashMap für die Key-Objekte zur Repräsentation der Tasten
 
     @Override
     public void initialize() {
@@ -18,15 +25,24 @@ public class SimpleKeyboard extends GraphicsApp {
     }
 
     private void initKeys() {
+        // Wir erstellen eine neue HashMap ...
         keys = new HashMap<>();
         float xOffset = 5;
+        // ... und iterieren mit einer For-Schleife über alle Buchstaben, die wir repräsentieren wollen ...
         for (int i = 'a'; i <= 'z'; i++) {
             char c = (char) i;
+            // ... und erstellen dabei für jeden Buchstaben ein passendes Key-Objekt, das in der Map gespeichert wird
             keys.put(c, new Key(xOffset, 5, c));
-            xOffset += Key.SIZE + 10;
+            xOffset += Key.SIZE + 10; // Wir erhöhen nach jedem Buchstaben die X-Position, an der der nächste Key eingezeichnet werden soll
         }
     }
 
+    /**
+     * Diese Methode sucht in der HashMap nach einem Wert, dessen Schlüssel dem übergebenen Parameter key entspricht. Falls ein solcher
+     * Wert gefunden wird (es handelt sich dann um ein Key-Objekt) wird die farbliche Hervorhebung dieses Rechtecks "umgeschaltet".
+     *
+     * @param key Char-Wert, zu dem der passende Key ("Rechteck") in der HashMap gesucht werden soll
+     */
     private void toggleKeyFocus(char key) {
         Key selectedKey = keys.get(key);
         if (selectedKey != null) {
@@ -37,6 +53,7 @@ public class SimpleKeyboard extends GraphicsApp {
     @Override
     public void draw() {
         drawBackground(Theme.GREY);
+        // Über die values-Methode der HashMap können wir einfach auf alle in der Map gespeicherten Werte zugreifen
         for (Key key : keys.values()) {
             key.draw();
         }
@@ -44,12 +61,12 @@ public class SimpleKeyboard extends GraphicsApp {
 
     @Override
     public void onKeyPressed(KeyPressedEvent event) {
-        toggleKeyFocus(event.getKeyChar());
+        toggleKeyFocus(event.getKeyChar()); // Beim Druck einer Taste wird in der HashMap nach dem korrespondierenden Key gesucht
     }
 
     @Override
     public void onKeyReleased(KeyReleasedEvent event) {
-        toggleKeyFocus(event.getKeyChar());
+        toggleKeyFocus(event.getKeyChar()); // Beim Loslassen einer Taste wird in der HashMap nach dem korrespondierenden Key gesucht
     }
 
     public static void main(String[] args) {
