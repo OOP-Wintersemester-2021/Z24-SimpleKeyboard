1b80146e42d2d394d83af22e9f7b1ccb85a19b4b
Alexander Bazo <alexanderbazo@googlemail.com>
2022-01-11 15:55:16 +0100

    Add starter code

---
+++ src/config/Settings.java
@@ -0,0 +1,8 @@
+package config;
+
+public abstract class Settings {
+
+    public final static float WINDOW_WIDTH = 1280;
+    public final static float WINDOW_HEIGHT = 720;
+
+}
---
+++ src/config/Theme.java
@@ -0,0 +1,16 @@
+package config;
+
+import de.ur.mi.oop.colors.Color;
+
+public abstract class Theme {
+
+    public static final Color RED = new Color(234, 49, 63); // "Selbstgemischter" RGB-Farbe (rot)
+    public static final Color YELLOW = new Color(234, 182, 56); // "Selbstgemischter" RGB-Farbe (gelb)
+    public static final Color GREEN = new Color(76, 149, 80); // "Selbstgemischter" RGB-Farbe (grün)
+    public static final Color BLUE = new Color(53, 129, 184); // "Selbstgemischter" RGB-Farbe (blau)
+    public static final Color TURQUOISE = new Color(9, 82, 86); // "Selbstgemischter" RGB-Farbe (türkis)
+    public static final Color PURPLE = new Color(55, 39, 114); // "Selbstgemischter" RGB-Farbe (lila)
+    public static final Color CREAM = new Color(241, 255, 250); // "Selbstgemischter" RGB-Farbe (creme)
+    public static final Color GREY = new Color(47, 61, 76); // "Selbstgemischter" RGB-Farbe (grau)
+
+}
